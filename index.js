ef828b1ee7185bb66ee169b8527c71ed36c09554
wilsonM15 <140748995+wilsonM15@users.noreply.github.com>
2024-07-30 15:07:35 +0200

    first commit
    possible changes coming soon....

---
+++ index.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    distance: '70px',
+    duration: 2000,
+    delay: 300,
+  });
+  
+  ScrollReveal().reveal('#image4, #image3 ', {origin: 'left'});
+  ScrollReveal().reveal('#image1, #image2 ', {origin: 'right'});
+  ScrollReveal().reveal('.', {origin: 'top'});
+  ScrollReveal().reveal('.container-square, .image-pair, .about-us-container, .menu-container, .sports-section, .reservations-section', {origin: 'bottom'});
+  
