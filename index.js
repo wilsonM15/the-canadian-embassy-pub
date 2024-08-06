707cd565914e3de6334cc7e99adcab1a9cdaa6b0
wilsonM15 <140748995+wilsonM15@users.noreply.github.com>
2024-08-06 15:57:32 +0200

    final commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ ScrollReveal({
     delay: 300,
   });
   
-  ScrollReveal().reveal('#image4, #image3 ', {origin: 'left'});
+  ScrollReveal().reveal('#image4, #image3, #image5', {origin: 'left'});
   ScrollReveal().reveal('#image1, #image2 ', {origin: 'right'});
   ScrollReveal().reveal('.', {origin: 'top'});
   ScrollReveal().reveal('.container-square, .image-pair, .about-us-container, .menu-container, .sports-section, .reservations-section', {origin: 'bottom'});
